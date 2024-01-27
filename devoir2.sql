f0144e8b10a11e50ba42c523a9f9df7bd27a58ad
ProfX1 <144231596+ProfX1@users.noreply.github.com>
2024-01-27 14:15:21 -0400

    Add files via upload

---
+++ devoir2.sql
@@ -0,0 +1,28 @@
+use library;
+-- Exercice 1 : À partir de la table « titles », obtenir la liste des titres contenant le mot « computer » Affiche le titre et le prix de chaque livre. 
+
+ select title, price from titles where title like '%computer%' group by title_id;
+
+-- Exercice 2 :À partir de la table « titles », obtenir la liste des titres contenant le mot « computer » (au singulier). Affiche le titre et le prix de chaque livre. (15 pts)
+
+ select title, price from titles where (title like '%computer%') and (title not like '%computers%') group by title_id;
+
+-- Exercice 3 : À partir de la table « titles », obtenir la liste des titres dont l’identifiant débute par SU ou BU. Affiche le titre et le prix de chaque livre. (15 pts)
+
+ select title, price from titles where titles.type rlike  '[bs]u'  group by title_id;
+
+-- Exercice 4 : À partir de la table « titles », obtenir la liste des titres dont l’identifiant ne débute pas par SU ou BU. Affiche le titre et le prix de chaque livre. (15 pts)
+
+ select title, price from titles where titles.type not rlike  '[bs]u'  group by title_id;
+
+-- Exercice 5 : À partir de la table « titles », obtenir la liste des titres ne débutant ni par S, ni par B, mais dont la seconde lettre est « o ». Affiche le titre et le prix. (15 pts)
+
+ select title, price from titles where titles.type not rlike  '[bs]' and titles.type like '_o%'  group by title_id;
+
+-- Exercice 6 : À partir de la table « titles », obtenir la liste des titres ne débutant ni par S, ni par B, mais dont la 3ième lettre est « f ». Affiche le titre et le prix. (15 pts)
+
+ select title, price from titles where titles.type not rlike  '[bs]' and titles.type like '__f%'  group by title_id;
+
+-- Exercice 7 : À partir de la table « titles », obtenir la liste des titres débutant par l’une des 10 premières lettres de l’alphabet. Affiche le titre et le prix. (15 pts)
+
+ select title, price from titles where (title rlike '^[a-j]')  group by title_id;
