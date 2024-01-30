6a93a717cc6d482fe95a12e7aa084662d4637319
ProfX1 <xavier.s.viel@gmail.com>
2024-01-30 10:37:23 -0400

    changementTypeATitle

--- devoir2.sql
+++ devoir2.sql
@@ -7,13 +7,13 @@ use library;
 
  select title, price from titles where (title like '%computer%') and (title not like '%computers%') group by title_id;
 
--- Exercice 3 : À partir de la table « titles », obtenir la liste des titres dont l’identifiant débute par SU ou BU. Affiche le titre et le prix de chaque livre. (15 pts)
+-- Exercice 3 : À partir de la table « titles », obtenir la liste des titres dont l’identifiant (titre ne fait pas de sens but ok) débute par SU ou BU. Affiche le titre et le prix de chaque livre. (15 pts)
 
- select title, price from titles where titles.type rlike  '[bs]u%'  group by title_id;
+ select title, price from titles where title rlike  '[bs]u%'  group by title_id;
 
--- Exercice 4 : À partir de la table « titles », obtenir la liste des titres dont l’identifiant ne débute pas par SU ou BU. Affiche le titre et le prix de chaque livre. (15 pts)
+-- Exercice 4 : À partir de la table « titles », obtenir la liste des titres dont l’identifiant (titre ne fait pas de sens but ok) ne débute pas par SU ou BU. Affiche le titre et le prix de chaque livre. (15 pts)
 
- select title, price from titles where titles.type not rlike  '[bs]u%'  group by title_id;
+ select title, price from titles where title not rlike  '[bs]u%'  group by title_id;
 
 -- Exercice 5 : À partir de la table « titles », obtenir la liste des titres ne débutant ni par S, ni par B, mais dont la seconde lettre est « o ». Affiche le titre et le prix. (15 pts)
 
