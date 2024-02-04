2971bb7e021a96886a5f20532ae6597b7e75156a
ProfX1 <xavier.s.viel@gmail.com>
2024-02-04 13:12:42 -0400

    exercice1

---
+++ devoir3.sql
@@ -0,0 +1,27 @@
+use library;
+
+-- 1. La liste des paires (auteur, éditeur) demeurant dans la même ville. (10pts)
+select pub_name, au_fname from library.publishers as p
+inner join 
+library.authors as a on a.city = p.city;
+
+
+-- 2. La liste des paires (auteur, éditeur) demeurant dans la même ville, incluant aussi les auteurs qui ne répondent pas à ce critère. (10pts)
+
+
+-- 3. La liste des paires (auteur, éditeur) demeurant dans la même ville, incluant aussi les éditeurs qui ne répondent pas à ce critère. (10pts)
+
+
+-- 4. La liste des paires (auteur, éditeur) demeurant dans la même ville, incluant les auteurs et les éditeurs qui ne répondent pas à ce critère. (10pts)
+
+
+-- 5. Effectif(nombre) d'employes par niveau d'experience (15pts)
+
+
+-- 6. Liste des employes par maison d'edition (15pts)
+
+
+-- 7. Salaires horaires moyens des employes par maison d'edition (15pts)
+
+
+-- 8- Effectif(nombre) d'employées de niveau SEINIOR par maison d'edition
