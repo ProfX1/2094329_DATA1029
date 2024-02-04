f4639cf62249b54c6bcc943ddfe3465d6427714e
ProfX1 <xavier.s.viel@gmail.com>
2024-02-04 16:10:18 -0400

    exercice4

--- devoir3.sql
+++ devoir3.sql
@@ -18,7 +18,15 @@ select pub_name, au_fname from library.publishers as p
 left join 
 library.authors as a on (a.city = p.city);
 
+-- 4. La liste des paires (auteur, éditeur) demeurant dans la même ville, incluant les auteurs et les éditeurs qui ne répondent pas à ce critère. (10pts)
 
+select pub_name, au_fname from library.authors as a
+left join 
+library.publishers as p on a.city = p.city 
+UNION
+select pub_name, au_fname from library.publishers as p
+left join 
+library.authors as a on (a.city = p.city);
 -- 5. Effectif(nombre) d'employes par niveau d'experience (15pts)
 
 
