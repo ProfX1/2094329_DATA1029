2f9511bb97ab60c6efd44c41938b4b1609189fa2
ProfX1 <xavier.s.viel@gmail.com>
2024-02-13 22:35:57 -0400

    modificationExercice2

--- devoir4.sql
+++ devoir4.sql
@@ -2,30 +2,25 @@ USE library;
 
 -- Exercice 1 Obtenir la liste des auteurs dont l’éditeur « Harmattan » n’a publié aucun livre
 
-SELECT a.au_fname 
+SELECT a.au_fname, a.au_lname 
 from authors as a 
 where a.au_id  not in (
-	SELECT distinct a.au_id
-	FROM authors as a 
-	INNER JOIN
-	library.titleauthor as ta, library.titles as t, library.publishers as p 
-    where ta.au_id = a.au_id and t.pub_id = p.pub_id and p.pub_name = "Harmattan");
+select au_id from titleauthor as ta
+WHERE ta.title_id in (select t.title_id from titles as t 
+JOIN 
+publishers as p on t.pub_id = p.pub_id
+WHERE pub_name="Harmattan") group by au_id);
 
 -- Exercice 2 Obtenir la liste des auteurs dont l’éditeur «Eyrolles » a publié tous les livres
 
 SELECT a.au_fname, a.au_lname
 FROM authors AS a
-WHERE "Eyrolles" in (
-    SELECT t.title_id
-    FROM titles AS t
-    LEFT JOIN publishers AS p ON t.pub_id = p.pub_id
-    WHERE p.pub_name = 'Eyrolles'
-    AND t.title_id NOT IN (
-        SELECT ta.title_id
-        FROM titleauthor AS ta
-        WHERE ta.au_id = a.au_id
-    )
-);
+WHERE a.au_id in
+(select au_id from titleauthor as ta
+WHERE ta.title_id in (select t.title_id from titles as t 
+JOIN 
+publishers as p on t.pub_id = p.pub_id
+WHERE pub_name="Eyrolles") group by au_id);
 
 -- Exercice 3  Obtenir la liste des noms d’auteurs ayant touché une avance supérieure à toutes les avances versées par l'éditeur "Harmattan"
 
