2ca344a4debfac8c1c7d8736ec873e4fb614758a
ProfX1 <xavier.s.viel@gmail.com>
2024-02-19 22:27:24 -0400

    sales

--- devoir5.sql
+++ devoir5.sql
@@ -26,19 +26,19 @@ CREATE TABLE authors(
   au_id TINYINt NOT NULL AUTO_INCREMENT PRIMARY KEY,
   au_lname VARCHAR(50) NOT NULL,
   au_fname VARCHAR(50) NOT NULL,
-  phone varchar(20) NULL,
+  phone varchar(20) not null UNIQUE CHECK (phone LIKE '+%' AND length(phone) BETWEEN 10 AND 15),
   address VARCHAR(50) NOT NULL,
   city VARCHAR(50) NOT NULL,
   state VARCHAR(50) NULL,
-  zip VARCHAR(6) NOT NULL,
+   zip VARCHAR(6) CHECK (zip regexp '^[A-Za-z][0-9][A-Za-z][0-9][A-Za-z][0-9]$') not NULL,
   contract text, 
   email varchar(50) NOT NULL unique,
   -- PRIMARY KEY (`au_id`),
   -- UNIQUE INDEX `au_id_UNIQUE` (`au_id` ASC) VISIBLE);
 );
 
-drop table IF exists Redactions;
-CREATE TABLE Redactions(
+drop table IF exists redactions;
+CREATE TABLE redactions(
 	au_id tinyint not null,
     title_id tinyint not null,
     au_ord tinyint not null,
@@ -48,3 +48,14 @@ CREATE TABLE Redactions(
     
 );
 
+DROP TABLE IF EXISTS sales; 
+create table sales (
+    store_id TINYINT ,
+    ord_num TINYINT,
+    title_id SMALLINT REFERENCES Titles(title_id),
+    ord_date datetime,
+    qty INT,
+    PRIMARY KEY (store_id, ord_num, title_id),
+    CONSTRAINT `fk_store_id` FOREIGN KEY (`store_id`) REFERENCES `stores` (`stor_id`),
+    CONSTRAINT `fk_title_id` FOREIGN KEY (`title_id`) REFERENCES `titles` (`title_id`)
+);
