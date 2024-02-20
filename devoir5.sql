66c0c44cae5c44b2ac8727d14f1d0116df5ce4fc
ProfX1 <xavier.s.viel@gmail.com>
2024-02-19 23:00:57 -0400

    employees

--- devoir5.sql
+++ devoir5.sql
@@ -4,21 +4,21 @@ CREATE database if not exists library2;
 use library2;
 DROP TABLE IF EXISTS jobs;
 CREATE TABLE jobs(
-job_id tinyint NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-job_desc varchar(50) UNIQUE NOT NULL, 
-min_lvl enum('STAGIAIRE','JUNIOR','INTERMEDIAIRE','SENIOR') NULL, 
-max_lvl enum('STAGIAIRE','JUNIOR','INTERMEDIAIRE','SENIOR') NULL
+	job_id tinyint NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+	job_desc varchar(50) UNIQUE NOT NULL, 
+	min_lvl enum('STAGIAIRE','JUNIOR','INTERMEDIAIRE','SENIOR') NULL, 
+	max_lvl enum('STAGIAIRE','JUNIOR','INTERMEDIAIRE','SENIOR') NULL
 -- CONSTRAINT pk_jobs PRIMARY KEY(job_id)
 -- CONSTRAINT uniq_job_desc UNIQUE(job_desc)
 );
 
 DROP TABLE IF EXISTS publishers;
 CREATE TABLE publishers(
-pub_id tinyint NOT NULL auto_increment primary key, 
-pub_name varchar(255) not null, 
-city varchar(255) not null, 
-state varchar(255) null,
-country varchar(255) not null
+	pub_id tinyint NOT NULL auto_increment primary key, 
+	pub_name varchar(255) not null, 
+	city varchar(255) not null, 
+	state varchar(255) null,
+	country varchar(255) not null
 );
 
 DROP TABLE IF EXISTS authors;
@@ -72,13 +72,27 @@ create table stores (
 
 DROP TABLE IF EXISTS titles;
 create table titles(
-tiles_id TINYINT AUTO_INCREMENT PRIMARY KEY,
-titles VARCHAR(100),
-`type` enum('Roman', 'Politique', 'Science', 'Histoire'),
-pub_id SMALLINT,
-price FLOAT,
-advance FLOAT,
-notes VARCHAR(255),
-pub_date DATE,
-CONSTRAINT `fk_pub_id` FOREIGN KEY (`pub_id`) REFERENCES `publishers` (`pub_id`)
+	tiles_id TINYINT AUTO_INCREMENT PRIMARY KEY,
+	titles VARCHAR(100),
+	`type` enum('Roman', 'Politique', 'Science', 'Histoire'),
+	pub_id SMALLINT,
+	price FLOAT,
+	advance FLOAT,
+	notes VARCHAR(255),
+	pub_date DATE,
+	CONSTRAINT `fk_pub_id` FOREIGN KEY (`pub_id`) REFERENCES `publishers` (`pub_id`)
+);
+
+DROP TABLE IF EXISTS employees;
+create table employees(
+	emp_id tinyint auto_increment primary key NOT NULL,
+    emp_name varchar(50) not NULL,
+    salary smallint not null,
+    fname varchar(50) not null,
+    lname varchar(50) not null,
+    job_id tinyint not null,
+    pub_date date not null,
+    email varchar(50) NOT NULL unique CHECK(email LIKE '%@%'),
+    CONSTRAINT `fk_job_id` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`job_id`),
+    CONSTRAINT `fk_pub_id` FOREIGN KEY (`pub_id`) REFERENCES `publishers` (`pub_id`)
 );
