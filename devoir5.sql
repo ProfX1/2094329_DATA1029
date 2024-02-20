09a7e2193ce3755fbe36504d05e29b00a600bb10
ProfX1 <xavier.s.viel@gmail.com>
2024-02-19 22:09:29 -0400

    redactions

--- devoir5.sql
+++ devoir5.sql
@@ -5,9 +5,9 @@ use library2;
 DROP TABLE IF EXISTS jobs;
 CREATE TABLE jobs(
 job_id tinyint NOT NULL AUTO_INCREMENT PRIMARY KEY, 
-job_desc varchar(255) UNIQUE NOT NULL, 
-min_lvl varchar(50) NULL, 
-max_lvl varchar(50) NULL
+job_desc varchar(50) UNIQUE NOT NULL, 
+min_lvl enum('STAGIAIRE','JUNIOR','INTERMEDIAIRE','SENIOR') NULL, 
+max_lvl enum('STAGIAIRE','JUNIOR','INTERMEDIAIRE','SENIOR') NULL
 -- CONSTRAINT pk_jobs PRIMARY KEY(job_id)
 -- CONSTRAINT uniq_job_desc UNIQUE(job_desc)
 );
@@ -23,15 +23,28 @@ country varchar(255) not null
 
 DROP TABLE IF EXISTS authors;
 CREATE TABLE authors(
-  au_id TINYINT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  au_lname VARCHAR(255) NOT NULL,
-  au_fname VARCHAR(255) NOT NULL,
-  phone INT NULL,
-  address VARCHAR(255) NOT NULL,
-  city VARCHAR(255) NOT NULL,
-  state VARCHAR(255) NULL,
-  zip VARCHAR(10) NOT NULL,
-  contract INT NULL
+  au_id TINYINt NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  au_lname VARCHAR(50) NOT NULL,
+  au_fname VARCHAR(50) NOT NULL,
+  phone varchar(20) NULL,
+  address VARCHAR(50) NOT NULL,
+  city VARCHAR(50) NOT NULL,
+  state VARCHAR(50) NULL,
+  zip VARCHAR(6) NOT NULL,
+  contract text, 
+  email varchar(50) NOT NULL unique,
   -- PRIMARY KEY (`au_id`),
   -- UNIQUE INDEX `au_id_UNIQUE` (`au_id` ASC) VISIBLE);
 );
+
+drop table IF exists Redactions;
+CREATE TABLE Redactions(
+	au_id tinyint not null,
+    title_id tinyint not null,
+    au_ord tinyint not null,
+    royalty float,
+    CONSTRAINT `fk_au_id` FOREIGN KEY (`au_id`) REFERENCES `authors` (`au_id`),
+	CONSTRAINT `fk_title_id` FOREIGN KEY (`title_id`) REFERENCES `titles` (`title_id`)
+    
+);
+
