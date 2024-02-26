c0ce052a7d24d67a5e194af330789ff15090438c
Xavier Viel <Xavier.s.viel@gmail.com>
2024-02-26 14:59:45 -0400

    question2.3

--- Mi-session/examen_mi-sessionXavier.sql
+++ Mi-session/examen_mi-sessionXavier.sql
@@ -9,7 +9,7 @@ epharmacy.role as r on r.id = u.id;
 -- Noms et quantités des produits achetés par Oumar Moussa.(20 points) :
 
 -- nom produit(product.name, product.id, ) ->product_id(orderLine), quantity, order_id -> order_id(Invoice), Customer_id ->id(user), full_name
-select `product`.`name`, orderline.quantity from product as p
+select p.name, orderline.quantity from product as p
 join
 epharmacy.orderline as o on o.product_id = p.product_id
 join
@@ -21,14 +21,18 @@ epharmacy.user as u on u.id = i.customer_id where u.full_name = "Oumar Moussa";
 
 -- Quel sont les noms de produits dont le fournisseur est basé à Moncton ? (25 points)
 
-select product.name from epharmacy.product as p
+select p.name from epharmacy.product as p
 join
-epharmacy.supplier as s on p.supplier_id = s.id where adress = "Moncton"
+epharmacy.supplier as s on p.supplier_id = s.id where adress = "Moncton";
 
 
--- Qui a passé le plus de temps une fois connecté dans l’application (10 points) ?
+-- Qui a passé le plus de temps une fois connecté dans l’application (10 points)
 
-
+select u.full_name from epharmacy.user as u
+join
+epharmacy.connection_history as c on u.id = c.user_id where max(onsite_time);
 
 
 -- Quel est le dernier utilisateur à se déconnecter ? (10 points)
+
+select
