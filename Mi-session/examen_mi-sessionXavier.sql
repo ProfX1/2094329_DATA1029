cac76fd62637a363e87052ff0b7f07fae7178f28
Xavier Viel <Xavier.s.viel@gmail.com>
2024-02-26 14:54:37 -0400

    question2.3

--- Mi-session/examen_mi-sessionXavier.sql
+++ Mi-session/examen_mi-sessionXavier.sql
@@ -8,12 +8,22 @@ epharmacy.role as r on r.id = u.id;
 
 -- Noms et quantités des produits achetés par Oumar Moussa.(20 points) :
 
+-- nom produit(product.name, product.id, ) ->product_id(orderLine), quantity, order_id -> order_id(Invoice), Customer_id ->id(user), full_name
+select `product`.`name`, orderline.quantity from product as p
+join
+epharmacy.orderline as o on o.product_id = p.product_id
+join
+epharmacy.invoice as i on i.order_id = o.order_id
+join
+epharmacy.user as u on u.id = i.customer_id where u.full_name = "Oumar Moussa";
 
 
 
 -- Quel sont les noms de produits dont le fournisseur est basé à Moncton ? (25 points)
 
-
+select product.name from epharmacy.product as p
+join
+epharmacy.supplier as s on p.supplier_id = s.id where adress = "Moncton"
 
 
 -- Qui a passé le plus de temps une fois connecté dans l’application (10 points) ?
